6896f84bde0316c5376bb969eee65f99c6f7374f
Joseph Daws <daws.joseph@gmail.com>
2020-12-15 22:57:53 -0500

    changing the way random policy is implemented. still incomplete

--- aicard/policy/random_policy.py
+++ aicard/policy/random_policy.py
@@ -3,10 +3,13 @@ import numpy as np
 
 class GoFishRandomPolicy:
     """Random policy for GoFish"""
-    def __init__(self, hand):
-        self.hand = hand
+    def __init__(self, observation, valid_actions):
+        self.observation = observation
 
     def rank_to_seek(self):
         """Choose a card whose rank you will to ask an opponent for."""
         ask_idx = np.random.randint(low=0, high=len(self.hand))
         return self.hand[ask_idx].rank
+
+    def player_to_ask(self):
+        return 0
