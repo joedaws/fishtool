7abc994891c4689ff319e5a84d9537738a6eb8e1
Joseph Daws <daws.joseph@gmail.com>
2020-12-15 20:26:28 -0500

    initial commit

---
+++ card_player/game/states.py
@@ -0,0 +1,50 @@
+from card_player.player import GoFishPlayer
+from card_player.deck import Deck
+from card_player.game.event import ExchangeEvent, BookEvent, DrawEvent
+from card_player.space import ObservationSpace
+
+
+class GoFishState:
+    """A class for storing data which represents the state of the go fish game."""
+    def __init__(self, num_players=4):
+        player_names = ['player'+str(i) for i in range(num_players)]
+        self.players = {i: GoFishPlayer(name) for i, name in enumerate(player_names)}
+        self._set_player_indices()
+        self.opponents_map = self._setup_opponents_map()
+
+        self.deck = Deck()
+
+        self.player_observations = {i: ObservationSpace(self.opponents_map[i])
+            for i in self.players.keys()}
+
+    def hands(self, player_indices=None):
+        """Returns hands of all of or a subset of players.
+
+        Args:
+            player_indicies (list): A list of integers corresponding to the hands
+                of the players which you want to see.
+        """
+        if player_indices is None:
+            # return all player hands
+            player_indices = list(self.players.keys())
+
+        return {i: player.hand for i, player in self.players.items() if i in player_indices}
+
+    def update(self, event):
+        """update the state according to the event."""
+        if isinstance(event, BookEvent):
+            pass
+        elif isinstance(event, ExchangeEvent):
+            pass
+        elif isinstance(event, DrawEvent):
+            pass
+
+    def _set_player_indices(self):
+        """Set the index attribute of the players."""
+        for index, player in self.players.items():
+            player.index = index 
+
+    def _setup_opponents_map(self):
+        """Create a dictionary describing the opponents of each player."""
+        indices = list(self.players.keys())
+        return {i: [j for j in indices if j != i] for i in indices}
