a06825cb1e9cbc0cfb42d2d0b9561cccb393ebde
Joseph Daws <joe@LIRIO-KNX-MBP001.local>
2020-12-18 11:06:51 -0500

    added call to play of game class to main

--- aicard/games/run.py
+++ aicard/games/run.py
@@ -4,44 +4,12 @@ import argparse
 
 
 def create_game(config):
+    """Imports and instantiates an instenace of the game specified """
     game_module = config['game']['game module']
     game_class = config['game']['game class']
     cls = getattr(import_module(game_module), game_class)
-    game_args = {'num_players':4}
-    print(cls)
-    game = cls(**game_args)
-    return game
-
-def main():
-    # make some players
-    p1 = GoFishPlayer('player1')
-    p2 = GoFishPlayer('player2')
-
-    # list of players for dealing
-    players = [p1, p2]
-
-    # make a deck
-    deck = Deck()
-
-    # deal cards to players
-    deal(deck, p1, p2)
-
-    for i in range(NUM_TURNS):
-        print(f"\n\nTurn {i}:")
-        play_turn(deck, p1, p2)
-        play_turn(deck, p2, p1)
-        print(f"{p1.name} has {len(p1.hand)} cards and {len(p1.books)} books")
-        #print(hand_str(p1))
-        print(f"{p2.name} has {len(p2.hand)} cards and {len(p2.books)} books.")
-        #print(hand_str(p2))
-        print(f"The deck has {len(deck.cards)} cards remaining.")
-
-        if len(p1.books) + len(p2.books) == 13:
-            if len(p1.books) > len(p2.books):
-                print(f"The game is over, {p1.name} wins!")
-            elif len(p2.books) > len(p1.books):
-                print(f"The game is over, {p2.name} wins!")
-            break
+    game_args = {'num_players': 4}
+    return cls(**game_args)
 
 
 if __name__ == "__main__":
@@ -60,4 +28,6 @@ if __name__ == "__main__":
     game = create_game(config)
     print(game.__dict__)
     print(f'Created {game.NAME}')
+
+    game.play()
 
