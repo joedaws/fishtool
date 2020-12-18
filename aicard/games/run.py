df38a49d0433c21fdc6ea58fdb5d972dd274ee1a
Joseph Daws <daws.joseph@gmail.com>
2020-12-18 10:25:12 -0500

    implemented configurable runs. Now game configs are passed as arguments.

--- aicard/games/run.py
+++ aicard/games/run.py
@@ -1,91 +1,16 @@
-from aicard.games.core.deck import Deck
-from aicard.players.go_fish import GoFishPlayer
-from aicard.brains.policies import GoFishRandomPolicy
+from importlib import import_module
+import configparser
+import argparse
 
 
-HAND_SIZE = 7
-NUM_TURNS = 30
-
-
-def show_state(player):
-    """Returns string representation of a players state"""
-    state_str = f"{player.name} state:\n"
-    for rank in player.state:
-        if player.state[rank]:
-            state_str += rank + ": "
-        for suit in player.state[rank]:
-            state_str += suit + " "
-        if player.state[rank]:
-            state_str += "\n"
-
-    return state_str
-
-
-def deal(deck, p1, p2):
-    """Deal cards from deck to p1 and p2"""
-    deck.shuffle()
-
-    # deal some cards
-    for _ in range(HAND_SIZE):
-        p1.draw(deck)
-        p2.draw(deck)
-
-    # show the state of each players's hand
-    print(show_state(p1))
-    print(show_state(p2))
-
-
-def play_turn(deck, player, opponent):
-    """Player asks opponent for a card and goes fish if no exchange."""
-    if len(player.hand) == 0:
-        if len(deck.cards) > 0:
-            player.draw(deck)
-
-    keep_asking = len(player.hand) > 0
-    """
-    if len(players.hand) > 0:
-        keep_asking = True
-    else:
-        keep_asking = False
-    """
-
-    asks = 0
-    while keep_asking:
-        # use policies to choose action
-        ask_rank = GoFishRandomPolicy(player.hand).rank_to_seek()
-
-        #rand_idx = randint(0, len(players.hand)-1)
-        #ask_rank = players.hand[rand_idx].rank
-        print(f"{player.name} is asking for a {ask_rank}")
-        keep_asking = player.ask(opponent, ask_rank)
-        asks += 1
-        if keep_asking:
-            print(f"{player.name} obtained at least one {ask_rank} from {opponent.name}.")
-        else:
-            print(f"{player.name} did not obtain a {ask_rank} from {opponent.name}.")
-
-        if len(player.hand) == 0:
-            if len(deck.cards) > 0:
-                player.draw(deck)
-            else:
-                keep_asking = False
-
-    if len(deck.cards) == 0:
-        print(f"The Deck is empty.")
-    elif asks == 1:
-        print(f"{player.name} is drawing a card.")
-        player.draw(deck)
-
-
-def hand_str(player):
-    """Returns single line string representation of hand"""
-    hand_str = ""
-
-    for c in player.hand:
-        hand_str += str(c) + " "
-
-    return hand_str
-
+def create_game(config):
+    game_module = config['game']['game module']
+    game_class = config['game']['game class']
+    cls = getattr(import_module(game_module), game_class)
+    game_args = {'num_players':4}
+    print(cls)
+    game = cls(**game_args)
+    return game
 
 def main():
     # make some players
@@ -120,4 +45,19 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser(description='Get path to the game config.')
+    parser.add_argument('--game_config',
+                         help='Path to the ini file configuring the game to be played.')
+    # parse arguements
+    args = parser.parse_args()
+    config_path = args.game_config
+
+    # load config
+    config = configparser.ConfigParser()
+    config.read(config_path)
+
+    # load game
+    game = create_game(config)
+    print(game.__dict__)
+    print(f'Created {game.NAME}')
+
